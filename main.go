49bd0b37c2647417200a4fd4cfaf8e33866c7e54
Steve McCoy <s@mccoy.space>
2019-09-29 10:44:22 -0400

    Flavicon

--- main.go
+++ main.go
@@ -221,6 +221,7 @@ var listPageTemplate = `<!DOCTYPE html>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<link rel="icon" href="style/favicon.png">
 	<link rel="stylesheet" href="style/stk5.css">
 
 	<title>WEBRSS</title>
